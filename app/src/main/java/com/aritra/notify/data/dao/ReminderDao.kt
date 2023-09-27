49ed38922b489f2bec81956b4c6eabde61c769ca
Aritra Das <aritrarick2002@gmail.com>
2023-09-27 15:56:21 +0530

    Created `Reminder Dao` interface 🐛

---
+++ app/src/main/java/com/aritra/notify/data/dao/ReminderDao.kt
@@ -0,0 +1,28 @@
+package com.aritra.notify.data.dao
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.aritra.notify.domain.models.Reminder
+import kotlinx.coroutines.flow.Flow
+
+interface ReminderDao {
+
+    @Query("SELECT * FROM reminder ORDER BY id ASC")
+    fun getAllReminders(): Flow<List<Reminder>>
+
+    @Query("SELECT * FROM reminder WHERE id = :reminderId")
+    fun getReminderById(reminderId: Int): Flow<Reminder>
+
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertReminder(reminderModel: Reminder): Long
+
+    @Update
+    suspend fun updateReminder(reminderModel: Reminder)
+
+    @Delete
+    suspend fun deleteReminder(reminderModel: Reminder)
+}
