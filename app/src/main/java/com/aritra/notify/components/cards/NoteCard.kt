4de462d39f25dc494e06b9b2a70f5b7d07f02301
Aritra Das <aritrarick2002@gmail.com>
2023-09-28 00:06:54 +0530

    Re-factored some files

--- app/src/main/java/com/aritra/notify/components/cards/NoteCard.kt
+++ app/src/main/java/com/aritra/notify/components/cards/NoteCard.kt
@@ -1,4 +1,4 @@
-package com.aritra.notify.components.note
+package com.aritra.notify.components.cards
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
